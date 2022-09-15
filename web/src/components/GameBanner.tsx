3a53271fd52c123a9be7a6db478fe82f96b9a3a7
Bruno Fukumori <brunofukumori@gmail.com>
2022-09-15 00:46:14 -0300

    add some additional styles to cards

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,7 +6,10 @@ interface GameBannerProps {
 
 export function GameBanner({ title, bannerUrl, adsCount }: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <a
+      href=""
+      className="relative rounded-lg overflow-hidden hover:scale-110 transition ease-in"
+    >
       <img src={bannerUrl} alt="" className="w-full" />
       <div className="absolute bottom-0 left-0 w-full pt-16 pb-4 px-4 bg-gradient-to-t from-black/90">
         <strong className="font-bold text-white block ">{title}</strong>
